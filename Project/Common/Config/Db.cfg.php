bb2725412bedaad355478988e5df2e2f8c42aa06
Your Name <you@example.com>
2017-10-18 09:19:12 +0800

    add version 2.0

---
+++ Project/Common/Config/Db.cfg.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * 数据库配置
+ */
+return [
+
+  /**
+   * 默认连接
+   */
+
+  'default' => [
+
+      'host' => '127.0.0.1',
+
+      'port' => 3306,
+
+      'username' => 'root',
+
+      'password' => 'root',
+
+      'database' => 'mysql',
+
+      'char' => 'utf8',
+
+      'dbType' => 'mysqli',
+
+      'connect' => false
+  ]
+
+];
