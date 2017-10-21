ef6f61450bfd4c59bf2dbf888a518690b743cf1d
Your Name <you@example.com>
2017-10-21 12:05:01 +0800

    add version 2.0

--- Framework/Library/Process/Extend.class.php
+++ Framework/Library/Process/Extend.class.php
@@ -23,6 +23,12 @@ class Extend
     public $ClassPath;
 
     /**
+     * 已加载的扩展容器
+     * @var string
+     */
+    public $Extendbox;
+
+    /**
      * 初始化相关路径
      * Extend constructor.
      */
@@ -40,8 +46,18 @@ class Extend
     public function addPackage($PackageName='')
     {
         if(!empty($PackageName) && file_exists($this->PackagePath . $PackageName)){
-            include_once $this->PackagePath . $PackageName;
+            $PackageName = $this->PackagePath . $PackageName;
+            $extension = self::get_extension($PackageName);
+            if(strtolower($extension)  == 'php'){
+                include_once $PackageName;
+                return true;
+            }
+            if(in_array($extension,['zip','tar'])){
+                $Packagezip = $this->getPackageName($PackageName);
+                $this->releasePackage($PackageName,'D:/111/',$Packagezip);
+            }
         }
+        return false;
     }
 
     /**
@@ -53,6 +69,86 @@ class Extend
         if(!empty($ClassName) && file_exists($this->ClassPath . $ClassName)){
             include_once $this->ClassPath . $ClassName;
         }
+        return false;
     }
 
+    /**
+     * 获取扩展信息
+     * @param $file
+     * @return mixed
+     */
+    public static function get_extension($file)
+    {
+        return pathinfo($file, PATHINFO_EXTENSION);
+    }
+
+    /**
+     * 获取zip包信息
+     */
+    private function getPackageInfo($infoPath='')
+    {
+        if(file_exists($infoPath)){
+            return include $infoPath;
+        }
+    }
+
+    /**
+     * 释放压缩文件
+     * @param string $zipfile
+     * @param string $folder
+     */
+    private function releasePackage($zipfile='',$folder='',$Packagezip)
+    {
+        if(class_exists('ZipArchive',false)){
+            $zip = new \ZipArchive;
+            $res = $zip->open($zipfile);
+            if ($res === TRUE) {
+                $zip->extractTo($folder);
+                $zip->close();
+
+                $autoload = $folder.$Packagezip.'/autoload.php';
+                if(file_exists($autoload)){
+                    include_once $autoload;
+                    $this->Extendbox[$Packagezip] = $this->getPackageInfo($folder.$Packagezip.'/info.php');
+                }
+            } else {
+                $error = [
+                    'file' => $zipfile,
+                    'message' => "'{$zipfile}' read file failure!"
+                ];
+                \Framework\App::$app->get('LogicExceptions')->readErrorFile($error);
+            }
+        }else{
+            $error = [
+                'file' => $zipfile,
+                'message' => "You need to start the PHP ZipArchive extension first!"
+            ];
+            \Framework\App::$app->get('LogicExceptions')->readErrorFile($error);
+        }
+    }
+
+
+    /**
+     * 返回包名
+     * @param $Package
+     * @return bool|mixed
+     */
+    private function getPackageName($Package)
+    {
+        $extension = self::get_extension($Package);
+        $path = explode('Package/',$Package);
+        if(isset($path[1])){
+            return str_replace(array('.',$extension),'',$path[1]);
+        }
+        return false;
+    }
+
+    /**
+     * 返回已加载的包信息
+     * @return string
+     */
+    public function getPackagebox()
+    {
+        return $this->Extendbox;
+    }
 }
