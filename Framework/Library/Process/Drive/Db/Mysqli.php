7c8d68b71e0c0637d4e05e98d063991cdf07788c
PC-20170618BUED\Administrator <php300@qq.com>
2017-12-15 11:39:06 +0800

    增加sql执行日志记录

--- Framework/Library/Process/Drive/Db/Mysqli.php
+++ Framework/Library/Process/Drive/Db/Mysqli.php
@@ -128,6 +128,8 @@ class Mysqli implements DbInterfaces
     {
         if ($this->link != null) {
             $this->queryId = mysqli_query($this->link, $queryString);
+            $status = $this->queryId !== false ? 'success' : 'error';
+            \Framework\App::$app->get('Log')->Record(\Framework\Library\Process\Running::$framworkPath .'/Project/Runtime/datebase','sql',"[{$status}] ".$queryString);
             if ($this->startsWith(strtolower($queryString), "select") && $select===false) {
                 $this->result = mysqli_fetch_all($this->queryId,MYSQLI_ASSOC);
                 return $this;
--- Framework/Library/Process/Drive/Db/PDO.php
+++ Framework/Library/Process/Drive/Db/PDO.php
@@ -178,6 +178,8 @@ class Pdo implements DbInterfaces
             $qry->execute();
             $qry->setFetchMode($method);
 
+            \Framework\App::$app->get('Log')->Record(\Framework\Library\Process\Running::$framworkPath .'/Project/Runtime/datebase','sql',$queryString);
+            
             if ($this->startsWith(strtolower($queryString), "select")) {
                 $this->result = $qry->fetchAll();
             }
