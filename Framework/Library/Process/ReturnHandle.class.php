3abc11bd04a088a0ff061e5ed9fc687f3aa7125b
PC-20170618BUED\Administrator <php300@qq.com>
2018-01-05 14:04:38 +0800

    更新控制器返回处理器自动编码处理

--- Framework/Library/Process/ReturnHandle.class.php
+++ Framework/Library/Process/ReturnHandle.class.php
@@ -12,59 +12,11 @@ class ReturnHandle
 
     /**
      * 处理返回值输出
-     * @param $Obj
+     * @param string $Obj
      */
-    public function Output($Obj)
+    public function Output($Obj = '')
     {
-       if(!empty($Obj)){
-           $ObjType = '';
-           if(is_object($Obj) && isset($Obj->_methon)){
-               $_methon = $Obj->_methon;
-               $ObjType = 1;
-               unset($Obj->_methon);
-           }
-           elseif(is_array($Obj) && isset($Obj['_methon'])){
-               $_methon = $Obj['_methon'];
-               $ObjType = 0;
-               unset($Obj['_methon']);
-           }
-           if(isset($_methon)){
-               switch($_methon){
-                   case 'json':
-                       echo json_encode($Obj);
-                       break;
-                   case 'join':
-                       if($ObjType === 1){
-                           echo implode(',',$Obj);
-                       }else{
-                          echo $this->joinObj(',',$Obj);
-                       }
-                        break;
-                   case 'dump':
-                       var_dump($Obj);
-                       break;
-                   case 'print':
-                       print_r($Obj);
-                       break;
-               }
-           }
-       }
-    }
-
-    /**
-     * 合并对象
-     * @param string $glue
-     * @param $Obj
-     * @return string
-     */
-    private function joinObj($glue = ',',$Obj)
-    {
-        $str = '';
-        foreach($Obj as $value){
-            $str .= $value.$glue;
-        }
-        $str = rtrim($str,'.'.$glue);
-        return $str;
+        echo is_object($Obj) || is_array($Obj) ? json_encode($Obj) : $Obj;
     }
 
 }
